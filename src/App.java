6ad4f4dce46592465afda75a9c4413e977e9dd0c
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2025-09-02 12:00:26 +0200

    börja

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        int biljetter = 10;
+
+        System.out.println("Välj attraktion");
+        System.out.println("1.Pumpen");
+        System.out.println("2.Flygande mattan");
+        System.out.println("3.Cirkuskarusellen");
+        System.out.println("4.Gå hem");
+
+        int val = scanner.nextInt();
+
+
+        
+    }
+}
