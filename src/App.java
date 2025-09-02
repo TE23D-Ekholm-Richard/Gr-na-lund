83b4f6d7dc0e63d4e206de1ec58bb36185d9cd85
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2025-09-02 12:44:41 +0200

    ez

--- src/App.java
+++ src/App.java
@@ -5,15 +5,17 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
         int biljetter = 10;
+        boolean fortsätt  = true;
 
-        System.out.println("Välj attraktion");
+   while(fortsätt){   
+    System.out.println("Välj attraktion");
         System.out.println("1.Pumpen");
         System.out.println("2.Flygande mattan");
         System.out.println("3.Cirkuskarusellen");
         System.out.println("4.Gå hem");
 
         int val = scanner.nextInt();
-
+    
 switch (val) {
     case 1:{
     System.out.println("hur lång e du");
@@ -45,7 +47,7 @@ case 2:
     if ( längd2 >= 120 && ålder >=7){
         if(biljetter >=2){
             biljetter -=2;
-            System.out.println("Du får åka du har -2"+ biljetter);
+            System.out.println("Du får åka du har " + biljetter + "kvar");
         }
         else{
             System.out.println("Inte tillräcklig kupinger");
@@ -54,9 +56,34 @@ case 2:
     else{
 System.out.println("To short buddy");
     }
+break;
 
-    }
+case 3:
+
+if(biljetter>=1){
+    biljetter-=1;
+    System.out.println("Du får åka du har  biljetter kvar" + biljetter);
+} else{
+    System.out.println("Broke behöver mer kuponger.");
 }
-        
+break;
+
+case 4: 
+System.out.println("Du går hem tack för idag");
+
+fortsätt=false;
+break;
+
+default:
+System.out.println("Fel val try again");
     }
+    if(biljetter==0){
+        System.out.println("Hej då du har slut med kuponger");
+        fortsätt = false;
+    }
+    
+}
+}
+    }
+    
 
